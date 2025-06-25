f4572e2c039e3a9eba33c07ea9116ff4494ebddc
Yassuan Vincenzo Foljanty Catanzaro <98028550+YayCover9000@users.noreply.github.com>
2025-06-25 14:42:59 +0200

    Delete IOrdering.java

--- IOrdering.java
+++
@@ -1,9 +0,0 @@
-package de.thb.dim.pizzaPronto;
-
-public abstract interface IOrdering {
-    public abstract OrderVO startNewOrder(CustomerVO customer);
-    public abstract void addDish(DishVO dish);
-    public abstract void deleteDish();
-    public abstract float calculateTotalPrice();
-    public abstract void confirmOrder();
-}
